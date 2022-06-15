09955e30693b59f9c2b5ef2159de7c00b2f94558
sfianchao <30313481+sfianchao@users.noreply.github.com>
2022-06-15 14:33:34 +0800

    deploy to master plan
    fuck

--- src/main/java/idsl/crosschain/transfer/config/QuorumConfig.java
+++ src/main/java/idsl/crosschain/transfer/config/QuorumConfig.java
@@ -5,14 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.web3j.crypto.Credentials;
-import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.quorum.Quorum;
-import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.tx.gas.StaticGasProvider;
 
-import java.io.File;
 import java.math.BigInteger;
 
 @Slf4j
@@ -110,121 +107,6 @@ public class QuorumConfig {
 //    <<總計劃>>
 //    --------------------------------------------------------------------------------------------------------------------
 
-//    @Bean(name = "quorumBuilder")
-//    public QuorumInfo quorumBuilder() {
-//
-//        QuorumInfo quorumInfo = new QuorumInfo();
-//
-//        try {
-//            // connect to blockchain
-//            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.214:9045"));
-//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
-//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
-//            log.info("client version: " + clientVersion);
-//
-//            // set up credentials
-//            Credentials credentials = Credentials.create("ba394d6ecde04efb7bce0001f130d878794d890b9da8a27eb950a4b6aa58dd76");
-//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
-//            log.info("account address: " + credentials.getAddress());
-//
-//            quorumInfo.setQuorum(quorum);
-//            quorumInfo.setCredentials(credentials);
-//            quorumInfo.setGasProvider(gasProvider);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return quorumInfo;
-//    }
-//
-//    @Bean(name = "sourceChainBuilder")
-//    public QuorumInfo sourceChainBuilder() {
-//
-//        QuorumInfo quorumInfo = new QuorumInfo();
-//
-//        try {
-//            // connect to blockchain
-//            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.214:9035"));
-//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
-//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
-//            log.info("[src chain] client version: " + clientVersion);
-//
-//            // set up credentials
-//            Credentials credentials = WalletUtils.loadCredentials("node1", new File("./wallets/wallet1"));
-//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.ZERO, DefaultGasProvider.GAS_LIMIT);
-//            log.info("[src chain] account address: " + credentials.getAddress());
-//
-//            quorumInfo.setQuorum(quorum);
-//            quorumInfo.setCredentials(credentials);
-//            quorumInfo.setGasProvider(gasProvider);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return quorumInfo;
-//    }
-//
-//    @Bean(name = "relayChainBuilder")
-//    public QuorumInfo relayChainBuilder() {
-//
-//        QuorumInfo quorumInfo = new QuorumInfo();
-//
-//        try {
-//            // connect to blockchain
-//            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.214:9045"));
-//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
-//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
-//            log.info("[relay chain] client version: " + clientVersion);
-//
-//            // set up credentials
-//            Credentials credentials = Credentials.create("ba394d6ecde04efb7bce0001f130d878794d890b9da8a27eb950a4b6aa58dd76");
-//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
-//            log.info("[relay chain] account address: " + credentials.getAddress());
-//
-//            quorumInfo.setQuorum(quorum);
-//            quorumInfo.setCredentials(credentials);
-//            quorumInfo.setGasProvider(gasProvider);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return quorumInfo;
-//    }
-//
-//    @Bean(name = "destinationChainBuilder")
-//    public QuorumInfo destinationChainBuilder() {
-//
-//        QuorumInfo quorumInfo = new QuorumInfo();
-//
-//        try {
-//            // connect to blockchain
-//            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.214:9055"));
-//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
-//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
-//            log.info("[dest chain] client version: " + clientVersion);
-//
-//            // set up credentials
-//            Credentials credentials = Credentials.create("4db25a3a7b7953809d61fca3d3ae13b2fc1498d5d246e93e4e8d959aef52f508");
-//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
-//            log.info("[dest chain] account address: " + credentials.getAddress());
-//
-//            quorumInfo.setQuorum(quorum);
-//            quorumInfo.setCredentials(credentials);
-//            quorumInfo.setGasProvider(gasProvider);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return quorumInfo;
-//    }
-
-//    <<idsl>>
-//    --------------------------------------------------------------------------------------------------------------------
-
     @Bean(name = "quorumBuilder")
     public QuorumInfo quorumBuilder() {
 
@@ -232,7 +114,7 @@ public class QuorumConfig {
 
         try {
             // connect to blockchain
-            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.74:8545"));
+            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.226:23001"));
             Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
             String clientVersion = web3ClientVersion.getWeb3ClientVersion();
             log.info("client version: " + clientVersion);
@@ -260,13 +142,13 @@ public class QuorumConfig {
 
         try {
             // connect to blockchain
-            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.73:8545"));
+            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.225:23001"));
             Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
             String clientVersion = web3ClientVersion.getWeb3ClientVersion();
             log.info("[src chain] client version: " + clientVersion);
 
             // set up credentials
-            Credentials credentials = Credentials.create("812d049c2ebb6678173680b6b77c1e79a3522709a2c9a5c051f6a3364e4b3698");
+            Credentials credentials = Credentials.create("d4659c3f7888bf8a5d3e2313f9e79fb141ab408f4f217adef18190ed98a2fff5");
             StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
             log.info("[src chain] account address: " + credentials.getAddress());
 
@@ -288,7 +170,7 @@ public class QuorumConfig {
 
         try {
             // connect to blockchain
-            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.74:8545"));
+            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.226:23001"));
             Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
             String clientVersion = web3ClientVersion.getWeb3ClientVersion();
             log.info("[relay chain] client version: " + clientVersion);
@@ -316,7 +198,7 @@ public class QuorumConfig {
 
         try {
             // connect to blockchain
-            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.75:8545"));
+            Quorum quorum = Quorum.build(new HttpService("http://140.118.9.227:23001"));
             Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
             String clientVersion = web3ClientVersion.getWeb3ClientVersion();
             log.info("[dest chain] client version: " + clientVersion);
@@ -336,5 +218,120 @@ public class QuorumConfig {
 
         return quorumInfo;
     }
+
+//    <<idsl>>
+//    --------------------------------------------------------------------------------------------------------------------
+
+//    @Bean(name = "quorumBuilder")
+//    public QuorumInfo quorumBuilder() {
+//
+//        QuorumInfo quorumInfo = new QuorumInfo();
+//
+//        try {
+//            // connect to blockchain
+//            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.74:8545"));
+//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
+//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+//            log.info("client version: " + clientVersion);
+//
+//            // set up credentials
+//            Credentials credentials = Credentials.create("ba394d6ecde04efb7bce0001f130d878794d890b9da8a27eb950a4b6aa58dd76");
+//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
+//            log.info("account address: " + credentials.getAddress());
+//
+//            quorumInfo.setQuorum(quorum);
+//            quorumInfo.setCredentials(credentials);
+//            quorumInfo.setGasProvider(gasProvider);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return quorumInfo;
+//    }
+//
+//    @Bean(name = "sourceChainBuilder")
+//    public QuorumInfo sourceChainBuilder() {
+//
+//        QuorumInfo quorumInfo = new QuorumInfo();
+//
+//        try {
+//            // connect to blockchain
+//            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.73:8545"));
+//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
+//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+//            log.info("[src chain] client version: " + clientVersion);
+//
+//            // set up credentials
+//            Credentials credentials = Credentials.create("812d049c2ebb6678173680b6b77c1e79a3522709a2c9a5c051f6a3364e4b3698");
+//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
+//            log.info("[src chain] account address: " + credentials.getAddress());
+//
+//            quorumInfo.setQuorum(quorum);
+//            quorumInfo.setCredentials(credentials);
+//            quorumInfo.setGasProvider(gasProvider);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return quorumInfo;
+//    }
+//
+//    @Bean(name = "relayChainBuilder")
+//    public QuorumInfo relayChainBuilder() {
+//
+//        QuorumInfo quorumInfo = new QuorumInfo();
+//
+//        try {
+//            // connect to blockchain
+//            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.74:8545"));
+//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
+//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+//            log.info("[relay chain] client version: " + clientVersion);
+//
+//            // set up credentials
+//            Credentials credentials = Credentials.create("ba394d6ecde04efb7bce0001f130d878794d890b9da8a27eb950a4b6aa58dd76");
+//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
+//            log.info("[relay chain] account address: " + credentials.getAddress());
+//
+//            quorumInfo.setQuorum(quorum);
+//            quorumInfo.setCredentials(credentials);
+//            quorumInfo.setGasProvider(gasProvider);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return quorumInfo;
+//    }
+//
+//    @Bean(name = "destinationChainBuilder")
+//    public QuorumInfo destinationChainBuilder() {
+//
+//        QuorumInfo quorumInfo = new QuorumInfo();
+//
+//        try {
+//            // connect to blockchain
+//            Quorum quorum = Quorum.build(new HttpService("http://192.168.66.75:8545"));
+//            Web3ClientVersion web3ClientVersion = quorum.web3ClientVersion().send();
+//            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+//            log.info("[dest chain] client version: " + clientVersion);
+//
+//            // set up credentials
+//            Credentials credentials = Credentials.create("4db25a3a7b7953809d61fca3d3ae13b2fc1498d5d246e93e4e8d959aef52f508");
+//            StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(20000000000L), BigInteger.valueOf(6721975L));
+//            log.info("[dest chain] account address: " + credentials.getAddress());
+//
+//            quorumInfo.setQuorum(quorum);
+//            quorumInfo.setCredentials(credentials);
+//            quorumInfo.setGasProvider(gasProvider);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return quorumInfo;
+//    }
 
 }
